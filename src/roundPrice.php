45fe17011928f31185591ccc509a7e275ec96a22
Pavel Ivanov <xeolit@gmail.com>
2021-10-31 21:21:01 +0300

    Task 2. Variable names were modified.

--- src/roundPrice.php
+++ src/roundPrice.php
@@ -5,30 +5,30 @@ namespace Kopirka;
 /*
  * Округление стоимости
  *
- * @param $num
+ * @param $value
  * @return float|int
  */
-function roundPrice($num) {
-    switch ($num) {
-        case $num >= 0 && $num <= 10 :
-            $multiply_by = 1;
+function roundPrice($value) {
+    switch ($value) {
+        case $value >= 0 && $value <= 10 :
+            $num = 1;
             break;
-        case $num > 10 && $num <= 100 :
-            $multiply_by = 5;
+        case $value > 10 && $value <= 100 :
+            $num = 5;
             break;
-        case $num > 100 && $num <= 500 :
-            $multiply_by = 10;
+        case $value > 100 && $value <= 500 :
+            $num = 10;
             break;
-        case $num > 500 && $num <= 1000 :
-            $multiply_by = 25;
+        case $value > 500 && $value <= 1000 :
+            $num = 25;
             break;
-        case $num > 1000 :
-            $multiply_by = 50;
+        case $value > 1000 :
+            $num = 50;
             break;
         default :
-            $multiply_by = 1;
+            $num = 1;
     }
 
-    $res = round($num / $multiply_by) * $multiply_by;
+    $res = round($value / $num) * $num;
     return $res;
 }
